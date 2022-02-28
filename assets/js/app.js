2990900d69ff9fca52cb8b0fb33d681ecddc8154
clinton <thisclinton@gmail.com>
2022-02-28 16:34:23 +0100

    working on the cart section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,47 @@
 const navToggle = document.getElementById("nav-toggle");
 const navClose = document.getElementById("nav-close");
-const navShow = document.getElementById("nav-menu");
+const navMenu = document.getElementById("nav-menu");
+const navLink = document.querySelectorAll('.nav__link');
+const cartToggle = document.getElementById("cart-toggle");
+const cartMenu = document.getElementById("cart-menu");
+const cartClose = document.getElementById("cart-close");
+const cartContent = document.getElementById("cart-content");
+const cartClear = document.getElementById("cart-clear");
+const cartTotal = document.getElementById("cart-total");
+const cartRemove = document.getElementById("cart-remove");
+
 
 if(navToggle){
     navToggle.addEventListener('click', () =>{
-        navShow.classList.add('show-menu')
+        navMenu.classList.add('show-menu')
     })
 }
 
 if(navClose){
     navClose.addEventListener('click', () =>{
-        navShow.classList.remove('show-menu')
+        navMenu.classList.remove('show-menu')
     })
 }
+
+function linkAction (){
+    const navMenu = document.getElementById('nav-menu')
+
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+if(cartToggle){
+    cartToggle.addEventListener('click', () =>{
+        cartMenu.classList.add('show-cart')
+    })
+}
+
+if(cartClose){
+    cartClose.addEventListener('click', () =>{
+        cartMenu.classList.remove('show-cart')
+    })
+}
+
+
